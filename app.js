37f7205e9229c7b5d6cdcf4af4f0ee538b831aa6
Kelder Passos <kelder.passos@gmail.com>
2022-08-15 11:58:02 -0300

    fix: lint

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-const express = require("express");
+const express = require('express');
 require('express-async-errors');
-const productRouter = require("./router/productRouter");
-const salesRouter = require("./router/salesRouter");
+const productRouter = require('./router/productRouter');
+const salesRouter = require('./router/salesRouter');
 
 const app = express();
 app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
-app.get("/", (_request, response) => {
+app.get('/', (_request, response) => {
   response.send();
 });
 
-app.use("/products", productRouter);
-app.use("/sales", salesRouter);
+app.use('/products', productRouter);
+app.use('/sales', salesRouter);
 app.use((error, req, res, _next) => {
   const { message } = error;
-  if (message === "Product not found") return res.status(404).json({ message });
+  if (message === 'Product not found') return res.status(404).json({ message });
 });
 
 // não remova essa exportação, é para o avaliador funcionar
