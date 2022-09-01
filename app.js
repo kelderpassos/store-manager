3920374947881d7dea1aa6afbde6f189fd0d0a4e
Kelder Passos <kelder.passos@gmail.com>
2022-09-01 12:24:15 -0300

    teste

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const salesRouter = require('./router/salesRouter');
 const app = express();
 app.use(express.json());
 
-// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
@@ -18,7 +17,4 @@ app.use((error, req, res, _next) => {
   if (message === 'Product not found') return res.status(404).json({ message });
 });
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
